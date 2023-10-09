6fc38da662cab8122cdfca461a3638f83706e2a4
venh <conf312@naver.com>
2023-10-09 11:04:41 +0900

    필드명 정리

--- src/main/java/com/issuemoa/batch/domain/board/Board.java
+++ src/main/java/com/issuemoa/batch/domain/board/Board.java
@@ -3,6 +3,7 @@ package com.issuemoa.batch.domain.board;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -12,35 +13,25 @@ public class Board {
     private String id;
     private String type;
     private String startDate;
-    private String endDate;
     private String allTimeYn;
     private String title;
     private String contents;
     private String url;
     private Long readCount;
-    private Long registerId;
-    private Long modifyId;
+    private String register;
+    private LocalDateTime registerDateTime;
 
     @Builder
-    public Board(String id, String type, String startDate, String endDate, String allTimeYn, String title, String contents, String url, Long readCount, Long registerId, Long modifyId) {
+    public Board(String id, String type, String startDate, String allTimeYn, String title, String contents, String url, Long readCount) {
         this.id = id;
         this.type = type;
         this.startDate = startDate;
-        this.endDate = endDate;
         this.allTimeYn = allTimeYn;
         this.title = title;
         this.contents = contents;
         this.url = url;
-        this.readCount = readCount;
-        this.registerId = registerId;
-        this.modifyId = modifyId;
-    }
-
-    public Board(String type, String allTimeYn, String title, String contents, String url) {
-        this.type = type;
-        this.allTimeYn = allTimeYn;
-        this.title = title;
-        this.contents = contents;
-        this.url = url;
+        this.readCount = 0L;
+        this.register = "batch";
+        this.registerDateTime = LocalDateTime.now();
     }
 }
