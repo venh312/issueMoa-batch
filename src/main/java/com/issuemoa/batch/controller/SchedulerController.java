96f12f3c2eb2a87ff3bf3aa3dd0e5fb3722e20e3
venh <conf312@naver.com>
2023-10-09 11:05:12 +0900

    배치 수동 실행 임시 컨트롤러 생성

---
+++ src/main/java/com/issuemoa/batch/controller/SchedulerController.java
@@ -0,0 +1,25 @@
+package com.issuemoa.batch.controller;
+
+import com.issuemoa.batch.scheduler.Scheduler;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class SchedulerController {
+    private final Scheduler scheduler;
+
+    @GetMapping("/execute/{type}")
+    public void executeScheduler(@PathVariable String type) {
+        log.info("[executeScheduler] => {}", type);
+        if (type.equals("news")) {
+            scheduler.startJobNaverNewsRank();
+        } else if (type.equals("youtube")) {
+            scheduler.startJobYoutubePopular();
+        }
+    }
+}
