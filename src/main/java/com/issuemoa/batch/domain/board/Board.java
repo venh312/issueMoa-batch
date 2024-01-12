7209ecc6a074acea6d1e406f7bba77ec7c50ad15
venh <conf312@naver.com>
2024-01-12 21:54:52 +0900

    [ADD] favoriteUserIds 관심 유저 ID 목록

--- src/main/java/com/issuemoa/batch/domain/board/Board.java
+++ src/main/java/com/issuemoa/batch/domain/board/Board.java
@@ -1,9 +1,12 @@
 package com.issuemoa.batch.domain.board;
 
 import lombok.*;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -11,23 +14,24 @@ import java.time.LocalDateTime;
 @Document(collection = "board")
 public class Board {
     @MongoId
-    private String id;
+    private ObjectId id;
     private String type;
     private String title;
     private String contents;
     private String url;
     private String thumbnail;
+    private List<String> favoriteUserIds;
     private String register;
     private LocalDateTime registerDateTime;
 
     @Builder
-    public Board(String id, String type, String title, String contents, String url, String thumbnail) {
-        this.id = id;
+    public Board(String type, String title, String contents, String url, String thumbnail) {
         this.type = type;
         this.title = title;
         this.contents = contents;
         this.url = url;
         this.thumbnail = thumbnail;
+        this.favoriteUserIds = new ArrayList<>();
         this.register = "BATCH";
         this.registerDateTime = LocalDateTime.now();
     }
